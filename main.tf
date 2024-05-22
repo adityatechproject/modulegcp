4aa6e563ceead0a9a8b7dc8994c33ab52214daa0
Adityatechproject <adityapmp@gmail.com>
2024-05-22 15:53:13 -0500

    project key added

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@
 
 resource "google_storage_bucket" "terraform-423501_bucket" {
   name                     = "terraform-423501_test"
+  project                  = "terraform-423501"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
