54d264e122a09c044db47a219a2d7cc9fa84de35
Adityatechproject <adityapmp@gmail.com>
2024-05-22 14:59:16 -0500

    format code

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 # module "vm" {
-  
+
 #   source       = "git@github.com:adityatechproject/modulegcp.git"
-  
+
 #   //source       = "../../Modulegcp/vm"
 
 # }
 
 
 resource "google_storage_bucket" "terraform-423501_bucket" {
-    name   = "terraform-423501_test"
-    location = "US"
-    force_destroy = true
-    public_access_prevention = "enforced"
-  
+  name                     = "terraform-423501_test"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+
 }
