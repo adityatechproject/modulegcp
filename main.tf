691b75e86b5a15eeec8ef5ba2127be305dadf6ee
Adityatechproject <adityapmp@gmail.com>
2024-05-22 14:46:25 -0500

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
-module "vm" {
+# module "vm" {
   
-  source       = "git@github.com:adityatechproject/modulegcp.git"
+#   source       = "git@github.com:adityatechproject/modulegcp.git"
   
-  //source       = "../../Modulegcp/vm"
+#   //source       = "../../Modulegcp/vm"
 
+# }
+
+
+resource "google_storage_bucket" "terraform-423501_bucket" {
+    name   = "terraform-423501_test"
+    location = "US"
+    force_destroy = true
+    public_access_prevention = "enforced"
+  
 }
