e6dd18aac00ecac0cc5d7b9098307d005d0c5893
Adityatechproject <adityapmp@gmail.com>
2024-05-23 14:04:51 -0500

    calling from module

--- modules/bucket.tf
+++ modules/bucket.tf
@@ -1,8 +1,9 @@
-resource "google_storage_bucket" "terraform-423501_bucket" {
+resource "google_storage_bucket" "terraform-423501_bucket_1" {
   name                     = "terraform-423501_test"
   project                  = "terraform-423501"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+
 
 }
