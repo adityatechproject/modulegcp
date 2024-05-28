b4e2808f7088a4c89491c3828047a9e813ee31e3
adityatechproject <adityapmp@gmail.com>
2024-05-28 15:13:06 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "bucket" {
+
+  //source = "git@github.com:adityatechproject/modulegcp.git"
+
+  source       = "./modules/"
+
+}
+
+
+# resource "google_storage_bucket" "terraform-423501_bucket" {
+#   name                     = "terraform-423501_test"
+#   project                  = "terraform-423501"
+#   location                 = "US"
+#   force_destroy            = true
+#   public_access_prevention = "enforced"
+
+# }
