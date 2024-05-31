62c3c9b33e47351f16899c0359b3047c73c7eaba
Adityatechproject <adityapmp@gmail.com>
2024-05-31 14:14:29 -0500

    test

--- bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "terraform-423501_bucket_1" {
-  name                     = "terraform-423501_test_1"
-  project                  = "terraform-423501"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-
-
-}
