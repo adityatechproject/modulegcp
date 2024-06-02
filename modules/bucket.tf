93fcc849ec16fd6316d3747ef05aa673ae84f625
adityatechproject <adityapmp@gmail.com>
2024-06-01 19:01:39 -0500

    Update bucket.tf

--- modules/bucket.tf
+++ modules/bucket.tf
@@ -1,9 +1,4 @@
 resource "google_storage_bucket" "terraform-423501_bucket_modulegcp" {
-  name                     = "terraform-423501_test_1"
+  name                     = "terraform-423501_bucket_modulegcp"
   project                  = "terraform-423501"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-
-
-}
+ }
