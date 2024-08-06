58baef63434b4b5077f258103c6a52c63ccd2f0d
adityatechproject <adityapmp@gmail.com>
2024-08-06 09:36:54 -0500

    Update vminstance.tf

--- modules/vminstance.tf
+++ modules/vminstance.tf
@@ -1,3 +1,31 @@
-module "instance-module" {
+resource "google_compute_instance" "vminstance" {
+  name         = "instance-4"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "bootdisk"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+
+
+  }
+  tags = ["linux"]
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "1081417027233-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
 
 }
